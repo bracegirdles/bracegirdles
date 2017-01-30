404d9fd7b7cf6d7eaec89451ef293c38f7f7a27b
anthonypecchillo <anthonypecchillo@gmail.com>
2017-01-30 15:12:14 -0800

    Refactored to ES6 syntax.  (Still need to refactor 'require's to 'import's later)

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.status(200).send('Status Code: 200' + '<br> <br>' + 'Victory.');
 });
 
-var server = app.listen(3000, '127.0.0.1', function () {
+const server = app.listen(3000, '127.0.0.1', () => {
   // console.log('This is what server.address() does: ', server.address());
   // console.log(server);  
-  var port = server.address().port;
+  const port = server.address().port;
   console.log('Basic server for HRAN listening at port', port);
 });
 
