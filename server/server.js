f8208be2a8bbc874a13ddd76eb00486cb9d25cd2
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-03 12:58:53 -0800

    (feat) Server Side Rendering: Set 'views' to server/views directory, set view engine to pug, and set static assets directory to dist.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
+// Include Modules:
 const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
 const app = express();
 
+// Server Side Rendering:
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Static files to server:
+console.log(path.resolve('dist'));
+app.use(express.static(path.resolve('dist')));
+
+// Connect to the Database:
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'user',
+//   password: 'password'
+// });
+
+// Query database and set the database we will be using:
+// connection.query('USE alumnidb');
+
 app.get('/', (req, res) => {
-  res.status(200).send('Status Code: 200' + '<br> <br>' + 'Victory.');
+  res.render('index');
 });
+
+// more of above to come...
 
 const server = app.listen(3000, '127.0.0.1', () => {
   // console.log('This is what server.address() does: ', server.address());
