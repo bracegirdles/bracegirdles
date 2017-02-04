9312226eb45a6989db61d4de6fcb1f74d4210e81
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-02-04 13:52:24 -0800

    (feat) Add host, dialect and pool properties in the database

--- db/db.js
+++ db/db.js
@@ -3,7 +3,15 @@ const Sequelize = require('sequelize');
 // Create a database with the name alumnidb
 // Serve with the username of root
 // No password for now !!
-let db = new Sequelize('alumnidb', 'root', '');
+let db = new Sequelize('alumnidb', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+  	max: 5,
+  	min: 0,
+  	idle: 10000
+  }
+});
 
 // Test database connection
 db.authenticate()
