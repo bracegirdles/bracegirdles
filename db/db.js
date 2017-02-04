759ffc131043ab7e6f29fc684904419b4c735924
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-04 11:37:35 -0800

    (cleanup) Add comment above db.authenticate()

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 
-//Create a database with the name alumnidb
-//Serve with the username of root
-//No password for now !!
+// Create a database with the name alumnidb
+// Serve with the username of root
+// No password for now !!
 let db = new Sequelize('alumnidb', 'root', '');
 
+// Test database connection
 db.authenticate()
   .then( () => { console.log('Connection successful!'); })
   .catch( err => { console.error('Unable to connect', err); });
