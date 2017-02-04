e5e9e0f82e124adcd1a653851ef1589dff6f6bf6
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-04 11:26:22 -0800

    (cleanup) Change tabs to spaces.

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,25 +1,22 @@
 import db from '../db.js';
 import Post from './postModel';
 
-
 let User = db.define('User', {
   name: {
-  	type: Sequelize.STRING,
-  	allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
-  	type: Sequelize.STRING,
+    type: Sequelize.STRING,
     unique: true
   },
   cohort: Sequelize.STRING,
   status: Sequelize.STRING,
   github: {
-  	type: Sequelize.STRING,
+    type: Sequelize.STRING,
     unique: true
   }
-
 });
-
 
 //Create a userId column in the Post table;
 Post.belongsTo(User);
@@ -27,9 +24,8 @@ Post.belongsTo(User);
 //Enable association between user and post
 User.hasMany(Post);
 
-
 //Creates tables in MySql if they do not exist
-//The force drops any exissting tables beforehand
+//The force drops any existing tables beforehand
 User.sync({force: true});
 Post.sync({force: true});
 
