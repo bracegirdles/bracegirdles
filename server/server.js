8fb9e7370e7f125766db508ddaa12c62a4e720ff
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-04 11:32:30 -0800

    (fix/cleanup) Remove 'Connect to database' and 'Query databse' sections

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
+const db = require('../db/db.js');
 const app = express();
 
 // Server Side Rendering:
@@ -12,25 +13,23 @@ app.set('view engine', 'pug');
 console.log(path.resolve('dist'));
 app.use(express.static(path.resolve('dist')));
 
-// Connect to the Database:
-// let connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'user',
-//   password: 'password'
-// });
-
-// Query database and set the database we will be using:
-// connection.query('USE alumnidb');
-
+// ----------------------------------------------------------------------------
 app.get('/', (req, res) => {
+  connection.query('SELECT * FROM User', function(err, rows) {
+    if (err) { 
+      console.error(err);
+    } else {
+      console.log(rows);
+    }
+  });
   res.render('index');
 });
 
-// more of above to come...
+// More of above to come...
+// ----------------------------------------------------------------------------
 
+// Establish server connection at localhost:3000
 const server = app.listen(3000, '127.0.0.1', () => {
-  // console.log('This is what server.address() does: ', server.address());
-  // console.log(server);  
   const port = server.address().port;
   console.log('Basic server for HRAN listening at port', port);
 });
