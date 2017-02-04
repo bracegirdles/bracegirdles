1976406e55c0ef310334124548984fc35e532d71
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-04 11:35:24 -0800

    (feat) Use .authenticate() to test database connection

--- db/db.js
+++ db/db.js
@@ -1,10 +1,12 @@
-import { Sequelize } from 'sequelize';
-
+const Sequelize = require('sequelize');
 
 //Create a database with the name alumnidb
 //Serve with the username of root
 //No password for now !!
 let db = new Sequelize('alumnidb', 'root', '');
 
+db.authenticate()
+  .then( () => { console.log('Connection successful!'); })
+  .catch( err => { console.error('Unable to connect', err); });
 
-export default db;
+module.exports = db;
