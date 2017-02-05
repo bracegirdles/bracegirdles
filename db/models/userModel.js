56e0afa7de7324077dc43b8ff8cd8c11e8928ef3
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-05 14:38:23 -0800

    (fix) Update foreign key assignment from Posts to Users and .sync() command

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -32,21 +32,18 @@ let User = db.define('User', {
   }
 });
 
+//Enable association between user and post
+User.hasMany(Post);
+
 //Create a userId column in the Post table;
 //Set the forignKey property to point to the user
-Post.belongsTo(User);  
+Post.belongsTo(User);
 
 // Removed {foreignKey: 'userId'}) from line 37.  Was giving the following error:
 // Unhandled rejection SequelizeDatabaseError: ER_DUP_FIELDNAME: Duplicate column name 'UserId'
 // 
 // Now foreign key defaults to UserId
 
-//Enable association between user and post
-User.hasMany(Post);
-
-//Creates tables in MySql if they do not exist
-//The force drops any existing tables beforehand
-User.sync({force: true});
-Post.sync({force: true});
+// db.sync();     THIS WILL DROP ANY EXISTING TABLES, SO ONLY EXECUTE WHEN INITIALIZING THE DB.
 
 module.exports = User;
