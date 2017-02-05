21b3580fe7097cd72c85057b4b7480ef79b2be27
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-02-05 15:21:01 -0800

    (feat) Add controller routes to server

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,10 @@ const controller = require('./controller.js');
 
 //Router consumes the routes made available by Sequelize
 //To talk to the database
-router.get('/users',controller.methods.users.get);
-router.post('/users',controller.methods.users.post);
-router.get('/posts',controller.methods.users.get);
-router.post('/posts',controller.methods.users.post);
+router.get('/users',controller.users.get);
+router.post('/users',controller.users.post);
+router.get('/posts',controller.users.get);
+router.post('/posts',controller.users.post);
 
 //Export the router with the Sequelize routes
 module.exports = router;
