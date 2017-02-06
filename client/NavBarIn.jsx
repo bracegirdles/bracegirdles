a89707770e436b255d8580ecdc24cad37503ae1b
Jordan Medina <jordanwmedina@gmail.com>
2017-02-06 12:12:43 -0800

    (refactor) Move share Nav components to root of client

---
+++ client/NavBarIn.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavBarIn = () => (
+  <nav className="navbar navbar-default">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="/">Hack Reactor Alumni Network</a>
+      </div>
+      <div id="navbar" className="collapse navbar-collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="/feed">Home</a></li>
+          <li><a href="/profile">Profile</a></li>
+          <li><a href="/settings">Settings</a></li>
+          <li><a href="/logout">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default NavBarIn;
