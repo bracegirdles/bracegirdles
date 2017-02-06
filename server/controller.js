90cc91370ac5cd19a3ea22599484664b30fe7212
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-05 17:32:42 -0800

    (cleanup) Syntax cleanup.  Tabs to spaces, indentation.

--- server/controller.js
+++ server/controller.js
@@ -7,14 +7,13 @@ db.sync();
 module.exports = {
   posts: {
     get: (req, res) => {
-    //might need to pass in parameters in findall
-    Post.findAll()
+    //might need to pass in parameters in findall	
+      Post.findAll()
     //example of findAll parameter
     //findAll({where : {username : req.body.username}})
-    .then((message) => {
-      res.json(message)
-    })
-
+      .then((message) => {
+        res.json(message);
+      });
     },
     post: (req, res) => {
      //check for relation between user and post and pass it in
@@ -29,9 +28,9 @@ module.exports = {
           message.save();
         })
         .then(() => {
-      	  res.sendStatus(201);
-        })
-      })
+          res.sendStatus(201);
+        });
+      });
     }
   },
   users: {
@@ -40,26 +39,35 @@ module.exports = {
       // value as object and the parameters as key value pair
       User.findAll([params])
       .then((foundPosts) => {
-      	//This will be an array so looping will be necessary
-        res.json(foundPosts)
-      })
+        //This will be an array so looping will be necessary
+        res.json(foundPosts);
+      });
     },
     post: (req, res) => {
       User.create({
         name: req.body.username,
         email: req.body.email,
+<<<<<<< HEAD
         cohort:req.body.cohort,
         status: req.body.status,
+=======
+        cohort: req.body.cohort,
+        status: req.body.status, 
+>>>>>>> (cleanup) Syntax cleanup.  Tabs to spaces, indentation.
         github: req.body.github
       })
       .then((user) => {
-      	user.save();
+        user.save();
       })
       .then(() => {
+<<<<<<< HEAD
       	res.sendStatus(201);
       })
 
+=======
+        res.sendStatus(201);
+      });
+>>>>>>> (cleanup) Syntax cleanup.  Tabs to spaces, indentation.
     }
   }
-
 };
