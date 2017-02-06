ade5de6c54055f65ea5e4ecdc75bb744db305b58
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-06 15:00:19 -0800

    (fix) Edit landing page subtitle

--- client/LandingComponents/Landing.jsx
+++ client/LandingComponents/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => (
           <div className="col-lg-12">
             <div className="content">
               <h1>Hack Reactor Alumni Network</h1>
-              <h3>The Nuclear Network for Hack Reactor Alumni</h3>
+              <h3>A platform for leveraging your peer network to accelerate your growth as a software engineer.</h3>
               <hr />
               <a href="/signup"><button className="btn btn-lg btn-primary" type="text">Sign Up!</button></a>
             </div>
