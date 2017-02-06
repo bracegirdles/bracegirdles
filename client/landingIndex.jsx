aa05324090f4f9e4a92363b5956410ebee61ffde
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-06 13:36:22 -0800

    (feat) Import landing.css

--- client/landingIndex.jsx
+++ client/landingIndex.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Landing from './LandingComponents/Landing.jsx';
+import css from './css/landing.css';
 
 window.onload = () => {
   ReactDOM.render(<Landing />, document.getElementById('landing'));
