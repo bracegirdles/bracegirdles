d33305ef914bd3d07dd7565c4a0ac9be66026190
Jordan Medina <jordanwmedina@gmail.com>
2017-02-05 19:51:27 -0800

    (refactor) Update to CommonJS require style

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
-import User from '../db/models/userModel';
-import Post from '../db/models/postModel';
+const User = require('../db/models/userModel');
+const Post = require('../db/models/postModel');
 const db = require('../db/db.js');
 
 db.sync();
@@ -7,7 +7,7 @@ db.sync();
 module.exports = {
   posts: {
     get: (req, res) => {
-    //might need to pass in parameters in findall	
+    //might need to pass in parameters in findall
     Post.findAll()
     //example of findAll parameter
     //findAll({where : {username : req.body.username}})
@@ -18,7 +18,7 @@ module.exports = {
     },
     post: (req, res) => {
      //check for relation between user and post and pass it in
-     //as a param in findorcreate	
+     //as a param in findorcreate
       User.findOrCreate()
       .spread(() => {
         Post.create({
@@ -37,7 +37,7 @@ module.exports = {
   users: {
     get: (req, res) => {
       //Pass in parameters as object with where as property
-      // value as object and the parameters as key value pair	
+      // value as object and the parameters as key value pair
       User.findAll([params])
       .then((foundPosts) => {
       	//This will be an array so looping will be necessary
@@ -49,7 +49,7 @@ module.exports = {
         name: req.body.username,
         email: req.body.email,
         cohort:req.body.cohort,
-        status: req.body.status, 
+        status: req.body.status,
         github: req.body.github
       })
       .then((user) => {
@@ -58,10 +58,8 @@ module.exports = {
       .then(() => {
       	res.sendStatus(201);
       })
-    	
+
     }
   }
 
 };
-
-
