a1cef00f7215061c66dab6192d5764bd0aef3e95
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-05 17:33:29 -0800

    (cleanup) Syntax cleanup.  Spaces after commas.

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,10 @@ const controller = require('./controller.js');
 
 //Router consumes the routes made available by Sequelize
 //To talk to the database
-router.get('/users',controller.users.get);
-router.post('/users',controller.users.post);
-router.get('/posts',controller.users.get);
-router.post('/posts',controller.users.post);
+router.get('/users', controller.users.get);
+router.post('/users', controller.users.post);
+router.get('/posts', controller.users.get);
+router.post('/posts', controller.users.post);
 
 //Export the router with the Sequelize routes
 module.exports = router;
